332c542446760c2d442da0a61b6e5ac6bbb68019
Suyash Shrestha <sthasuyash11@gmail.com>
2022-05-04 15:59:36 +0545

    added Discrete Structures algorithm

---
+++ DS lab/floor_and_ceiling.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    double num, floored, ceiled;
+    char again;
+    printf("Enter a floating value: ");
+    scanf("%lf", &num);
+    floored = floor(num);
+    ceiled = ceil(num);
+    printf("The entered value is: %lf\nFloored value: %.0lf\nCeiled value: %.0lf", num, floored, ceiled);
+    return 0;
+}
