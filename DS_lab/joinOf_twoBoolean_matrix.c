101551b239ee32e6cdeff8b4ec0997f1a7c2f080
Suyash Shrestha <sthasuyash11@gmail.com>
2022-05-15 11:48:48 +0545

    made functions for repeated codes

--- DS_lab/joinOf_twoBoolean_matrix.c
+++ DS_lab/joinOf_twoBoolean_matrix.c
@@ -1,62 +1,51 @@
-/**** algorithm ( Join of two boolean matrices ) ****/
+/**** algorithm ( Meet of two boolean matrices ) ****/
 #include <stdio.h>
+
+int rows, columns; // declare these global variable as they are needed everywhere
+
+// function to scan values of matrices
+void scanMatrix(char matName, int mat[rows][columns])
+{
+    printf("Enter data for matrix %c:\n", matName);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            scanf("%d", &mat[i][j]);
+        }
+    }
+}
+
+// function to print data in matrix form
+void printMatrix(char matName, int mat[rows][columns])
+{
+    printf("\nMatrix %c:\n", matName);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            printf("%d\t", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
 
 int main(void)
 {
-    int rows, columns; // variables to store the number of rows and columns of matrix the user wants
+    // int rows, columns; // variables to store the number of rows and columns of matrix the user wants
     printf("Enter the rows for matrix: ");
     scanf("%d", &rows);
     printf("Enter the columns for matrix: ");
     scanf("%d", &columns);
-
     // make two arrays A and B with the provided rows and columns
     int A[rows][columns], B[rows][columns];
-
-    // entering data for matrix A
-    printf("\nEnter data for boolean matrix A:\n");
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < columns; j++)
-        {
-            printf("A[%d][%d]: ", i, j);
-            scanf("%d", &A[i][j]);
-        }
-    }
-
-    // entering data for matrix B
-    printf("\nEnter data for boolean matrix A:\n");
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < columns; j++)
-        {
-            printf("B[%d][%d]: ", i, j);
-            scanf("%d", &B[i][j]);
-        }
-    }
-
-    // displaying data of matrix A
-    printf("\nMatrix A:\n");
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < columns; j++)
-        {
-            printf("%d\t", A[i][j]);
-        }
-        printf("\n");
-    }
-
-    // displaying data of matrix B
-    printf("Matrix B:\n");
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < columns; j++)
-        {
-            printf("%d\t", B[i][j]);
-        }
-        printf("\n");
-    }
-
-    // displaying the join of the matrices A and B
+    // call functions to scan the matrix A and B
+    scanMatrix('A', A);
+    scanMatrix('B', B);
+    // displaying data in matrix form
+    printMatrix('A', A);
+    printMatrix('B', B);
+    // displaying the meet of the matrices A and B
     printf("\nJoin of matrices A and B:\n");
     for (int i = 0; i < rows; i++)
     {
